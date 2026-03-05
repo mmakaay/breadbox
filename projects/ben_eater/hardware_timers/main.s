7cebe061dce81504e22592f0e85d35d8888c35e1
Maurice Makaay <maurice@h2b.nl>
2026-03-05 01:04:29 +0100

    Use ticker timer slots in hardware_timers project
    Replace manual timestamp tracking and multi-byte comparison boilerplate
    with YAML-configured timer slots. Task code now just checks and clears
    a flag byte per timer.

--- projects/ben_eater/hardware_timers/main.s
+++ projects/ben_eater/hardware_timers/main.s
@@ -4,15 +4,6 @@
 
 .export main
 
-; The number of ticks to wait, for each of the tasks.
-LED_UPDATE_TICKS = 25  ; = 250ms
-LCD_UPDATE_TICKS = 300 ; = 3s (high, to demonstrate >255 ticks delays)
-
-.segment "RAM"
-
-    last_led_toggle: .res 1
-    last_lcd_update: .res 2
-
 .segment "DATA"
 
     message: .asciiz "BREADBOX HWticks"
@@ -26,10 +17,6 @@ LCD_UPDATE_TICKS = 300 ; = 3s (high, to demonstrate >255 ticks delays)
         jsr LCD::cursor_move
         PRINT LCD::write, message
 
-        ; Initialize the timing variables.
-        CP_BYTE last_led_toggle, TICKER::ticks
-        CP_WORD last_lcd_update, TICKER::ticks
-
         ; Run the execution loop, giving each task, in turn, a chance
         ; to perform some work.
     @loop:
@@ -39,42 +26,21 @@ LCD_UPDATE_TICKS = 300 ; = 3s (high, to demonstrate >255 ticks delays)
     .endproc
 
     .proc update_led_task
-        ; Check if we have reach the next action time.
-        sec
-        lda TICKER::ticks
-        sbc last_led_toggle
-        cmp #LED_UPDATE_TICKS
-        bcc @done
+        lda TICKER::led_toggle
+        beq @done
+        CLR_BYTE TICKER::led_toggle
 
-        ; Yes, record new toggle time and toggle the LED.
-        CP_BYTE last_led_toggle, TICKER::ticks
         jsr LED::toggle
     @done:
         rts
     .endproc
 
     .proc update_lcd_task
-        ; Check if we have reach the next action time.
-        ; Since we wait for >255 ticks, here we must look at two of
-        ; the ticker's counter bytes.
-        sec
-        lda TICKER::ticks
-        sbc last_lcd_update
-        tax
-        lda TICKER::ticks+1
-        sbc last_lcd_update+1
-        cmp #>LCD_UPDATE_TICKS
-        bcc @done
-        bne @update
-        txa
-        cmp #<LCD_UPDATE_TICKS
-        bcc @done
+        lda TICKER::lcd_update
+        beq @done
+        CLR_BYTE TICKER::lcd_update
 
-    @update:
-        ; Yes, record the new lcd time.
-        CP_WORD last_lcd_update, TICKER::ticks
-
-        ; And update the LCD.
+        ; Update the LCD with the current tick counter.
         jsr LCD::home
         CP_WORD fmtdec16::value, TICKER::ticks + 2
         jsr fmtdec16
